0266ebe4554994ac6b4c88c667f394b15ea27442
Priya Bhattacharya <priyabhattacharya2005@gmail.com>
2025-11-10 15:06:59 +0530

    Update Case-Insensitive Character Frequency with Sorting.java

--- Case-Insensitive Character Frequency with Sorting.java
+++ Case-Insensitive Character Frequency with Sorting.java
@@ -1,3 +1,11 @@
+Examples: 
+
+Input: s = "geeksforgeeks"
+Output: g2 e4 k2 s2 f1 o1 r1
+
+Input: str = "elephant"
+Output: e2 l1 p1 h1 a1 n1 t1
+=======================================
 import java.io.*;
 import java.util.*;
 
